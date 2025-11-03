fe025489b49690241732ce95b9f00c103f790944
Mridul Agrawal <mridulagarwal20082004@gmail.com>
2025-11-04 02:50:23 +0530

    Time: 15 ms (76.87%), Space: 46.9 MB (5.32%) - LeetHub

---
+++ 0322-coin-change/0322-coin-change.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int coinChange(int[] coins, int amount) {
+        int n = coins.length;
+        int INF = (int) 1e9;
+
+        int[][] dp = new int[n][amount + 1];
+
+        // Base Case: Fill for the first coin
+        for (int t = 0; t <= amount; t++) {
+            if (t % coins[0] == 0)
+                dp[0][t] = t / coins[0];
+            else
+                dp[0][t] = INF;
+        }
+
+        // Fill the DP table
+        for (int i = 1; i < n; i++) {
+            for (int t = 0; t <= amount; t++) {
+                int notTake = dp[i - 1][t];
+                int take = INF;
+                if (coins[i] <= t)
+                    take = 1 + dp[i][t - coins[i]];
+
+                dp[i][t] = Math.min(notTake, take);
+            }
+        }
+
+        int ans = dp[n - 1][amount];
+        return ans >= INF ? -1 : ans;
+    }
+}
